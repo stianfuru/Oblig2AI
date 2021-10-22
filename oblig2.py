24d33f30e7365b16c5bd64792554f69c399608b5
Stian Furu <stian.furu97@gmail.com>
2021-10-22 13:32:38 +0200

    la inn table

---
+++ oblig2.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+url = "https://raw.githubusercontent.com/umaimehm/Intro_to_AI_2021/main/assignment2-machinelearning/Ruter-data.csv"
+
+df = pd.read_csv(url, sep=";")
+
